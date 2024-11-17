34011d7d0e6ddf58249e4298a49b8929f218f164
Krishan <krishandwive9@gmail.com>
2024-11-18 01:01:25 +0530

    event creation working

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useCallback, Dispatch, SetStateAction } from "react";
-// import type { FileWithPath } from '@uploadthing/react'
 import { useDropzone } from "@uploadthing/react/hooks";
 import { generateClientDropzoneAccept } from "uploadthing/client";
 
@@ -26,9 +25,11 @@ export function FileUploader({
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    // accept: "image/*",
-
-    // accept: "image/*",
+    accept: generateClientDropzoneAccept([
+      "image/png",
+      "image/jpg",
+      "image/jpeg",
+    ]),
   });
 
   return (
